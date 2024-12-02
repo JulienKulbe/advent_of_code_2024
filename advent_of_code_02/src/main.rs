b000823f677e7195671ab6f5a01a643b0fd596c7
Julien Kulbe <julien.kulbe@googlemail.com>
2024-12-02 19:03:24 +0100

    day 2, star 2

--- advent_of_code_02/src/main.rs
+++ advent_of_code_02/src/main.rs
@@ -1,12 +1,12 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use std::{fs::read_to_string, num::ParseIntError};
 
 fn main() -> Result<()> {
     let reports = get_safe_reports("input.txt")?;
     println!("Safe reports: {reports}");
 
-    let score = calculate_similarity_score("input.txt")?;
-    println!("Similarity score: {score}");
+    let reports2 = get_safe_reports_with_dampener("input.txt")?;
+    println!("Similarity score: {reports2}");
 
     Ok(())
 }
@@ -17,8 +17,13 @@ fn get_safe_reports(filename: &str) -> Result<usize> {
     Ok(safe_reports)
 }
 
-fn calculate_similarity_score(filename: &str) -> Result<u32> {
-    Ok(1)
+fn get_safe_reports_with_dampener(filename: &str) -> Result<usize> {
+    let data = parse_file(filename)?;
+    let safe_reports = data
+        .iter()
+        .filter(|report| is_report_safe_with_dampener(report))
+        .count();
+    Ok(safe_reports)
 }
 
 fn parse_file(filename: &str) -> Result<Vec<Vec<u32>>> {
@@ -42,6 +47,27 @@ fn is_report_safe(report: &[u32]) -> bool {
         .iter()
         .zip(report.iter().skip(1))
         .all(|(prev, curr)| order.is_in_order(prev, curr))
+}
+
+fn is_report_safe_with_dampener(report: &[u32]) -> bool {
+    if is_report_safe(report) {
+        return true;
+    }
+
+    for index in 0..report.len() {
+        // create a new report without the n-th level
+        let new_report = report
+            .iter()
+            .enumerate()
+            .filter_map(|(i, e)| if i != index { Some(e) } else { None })
+            .cloned()
+            .collect::<Vec<_>>();
+        if is_report_safe(&new_report) {
+            return true;
+        }
+    }
+
+    false
 }
 
 enum Order {
@@ -83,22 +109,20 @@ mod tests {
     fn test_input_a() {
         let result = get_safe_reports("input.txt");
         assert!(result.is_ok());
-        //assert_eq!(1579939, result.unwrap())
+        assert_eq!(299, result.unwrap())
     }
 
     #[test]
-    #[ignore]
     fn test_small_b() {
-        let result = calculate_similarity_score("input_small.txt");
+        let result = get_safe_reports_with_dampener("input_small.txt");
         assert!(result.is_ok());
-        assert_eq!(31, result.unwrap())
+        assert_eq!(4, result.unwrap())
     }
 
     #[test]
-    #[ignore]
     fn test_input_b() {
-        let result = calculate_similarity_score("input.txt");
+        let result = get_safe_reports_with_dampener("input.txt");
         assert!(result.is_ok());
-        assert_eq!(20351745, result.unwrap())
+        assert_eq!(364, result.unwrap())
     }
 }
