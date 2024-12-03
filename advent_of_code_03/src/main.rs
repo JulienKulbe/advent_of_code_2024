c223129574ba72c04d2b0acd138b7d26632a43b1
Julien Kulbe <julien.kulbe@googlemail.com>
2024-12-03 18:17:21 +0100

    day 3, star 2, dirty regex

--- advent_of_code_03/src/main.rs
+++ advent_of_code_03/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use regex::Regex;
 use std::fs::read_to_string;
 
@@ -6,8 +6,8 @@ fn main() -> Result<()> {
     let sum = calculates_sum("input.txt")?;
     println!("Sum: {sum}");
 
-    let score = calculate_similarity_score("input.txt")?;
-    println!("Similarity score: {score}");
+    let sum = calculate_enabled_sum("input.txt")?;
+    println!("Enabled sum: {sum}");
 
     Ok(())
 }
@@ -24,8 +24,42 @@ fn calculates_sum(filename: &str) -> Result<u32> {
     Ok(sum)
 }
 
-fn calculate_similarity_score(filename: &str) -> Result<u32> {
-    Ok(1)
+fn calculate_enabled_sum(filename: &str) -> Result<u32> {
+    let haystack = parse_file(filename)?;
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")?;
+
+    let mut mults = Vec::new();
+    for (start, (_, [mult1, mult2])) in re
+        .captures_iter(&haystack)
+        .map(|c| (c.get(1).unwrap().start(), c.extract()))
+    {
+        mults.push((start, mult1.parse::<u32>()? * mult2.parse::<u32>()?));
+    }
+
+    let mut sum = 0;
+    for (start, mult) in mults {
+        // find position of last do() and don't() starting from the regex start position
+        let last_do = haystack[0..start].rfind("do()");
+        let last_dont = haystack[0..start].rfind("don't()");
+
+        // set mult to None if don't() was after the last do()
+        let mut mult = Some(mult);
+        if let Some(last_dont) = last_dont {
+            if let Some(last_do) = last_do {
+                if last_dont > last_do {
+                    mult = None
+                }
+            } else {
+                mult = None;
+            }
+        }
+
+        if let Some(mult) = mult {
+            sum += mult;
+        }
+    }
+
+    Ok(sum)
 }
 
 fn parse_file(filename: &str) -> Result<String> {
@@ -52,18 +86,16 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_small_b() {
-        let result = calculate_similarity_score("input_small.txt");
+        let result = calculate_enabled_sum("input_small_b.txt");
         assert!(result.is_ok());
-        assert_eq!(31, result.unwrap())
+        assert_eq!(48, result.unwrap())
     }
 
     #[test]
-    #[ignore]
     fn test_input_b() {
-        let result = calculate_similarity_score("input.txt");
+        let result = calculate_enabled_sum("input.txt");
         assert!(result.is_ok());
-        assert_eq!(20351745, result.unwrap())
+        assert_eq!(104083373, result.unwrap())
     }
 }
