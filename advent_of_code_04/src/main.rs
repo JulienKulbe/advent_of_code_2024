0eee86dea4e40a6f678f7495d146b4d2f06429c1
Julien Kulbe <julien.kulbe@googlemail.com>
2024-12-04 21:01:41 +0100

    refactored code

--- advent_of_code_04/src/main.rs
+++ advent_of_code_04/src/main.rs
@@ -31,25 +31,20 @@ fn count_x_mas(filename: &str) -> Result<usize> {
     for x in 0..map.width {
         for y in 0..map.height {
             let pos = Position(x, y);
-            if map.get(pos) == 'A' {
+            if map.get(pos) == Some('A') {
                 let top_left = map.go_to(pos, Direction::TopLeft);
                 let down_left = map.go_to(pos, Direction::DownLeft);
                 let top_right = map.go_to(pos, Direction::TopRight);
                 let down_right = map.go_to(pos, Direction::DownRight);
-                if top_left.is_some()
-                    && down_left.is_some()
-                    && top_right.is_some()
-                    && down_right.is_some()
-                {
-                    let mut diagonal1 = [map.get(top_left.unwrap()), map.get(down_right.unwrap())];
-                    let mut diagonal2 = [map.get(top_right.unwrap()), map.get(down_left.unwrap())];
 
-                    diagonal1.sort_unstable();
-                    diagonal2.sort_unstable();
+                let mut diagonal1 = [map.get(top_left), map.get(down_right)];
+                let mut diagonal2 = [map.get(top_right), map.get(down_left)];
 
-                    if diagonal1 == ['M', 'S'] && diagonal2 == ['M', 'S'] {
-                        sum += 1;
-                    }
+                diagonal1.sort_unstable();
+                diagonal2.sort_unstable();
+
+                if diagonal1 == [Some('M'), Some('S')] && diagonal2 == [Some('M'), Some('S')] {
+                    sum += 1;
                 }
             }
         }
@@ -64,39 +59,34 @@ fn count_xmas_from_pos(map: &Map, pos: Position) -> usize {
         .count()
 }
 
-fn contains_xmas(map: &Map, pos: Position, direction: Direction) -> bool {
+fn contains_xmas(map: &Map, mut pos: Position, direction: Direction) -> bool {
     let xmas = ['X', 'M', 'A', 'S'];
 
-    let mut current = Some(pos);
     for character in xmas {
-        if let Some(pos) = current {
-            if map.get(pos) != character {
-                return false;
-            }
-            current = map.go_to(pos, direction);
-        } else {
+        if map.get(pos) != Some(character) {
             return false;
         }
+        pos = map.go_to(pos, direction);
     }
 
     true
 }
 
 #[derive(Debug, Clone, Copy)]
-struct Position(usize, usize);
+struct Position(i32, i32);
 
 struct Map {
     data: Vec<Vec<char>>,
-    height: usize,
-    width: usize,
+    height: i32,
+    width: i32,
 }
 
 impl Map {
     fn new(filename: &str) -> Map {
         let file = read_to_string(filename).unwrap();
         let data: Vec<Vec<char>> = file.lines().map(|line| line.chars().collect()).collect();
-        let height = data.len();
-        let width = data.first().unwrap().len();
+        let height = data.len() as i32;
+        let width = data.first().unwrap().len() as i32;
 
         Map {
             data,
@@ -105,68 +95,26 @@ impl Map {
         }
     }
 
-    fn get(&self, position: Position) -> char {
-        self.data[position.0][position.1]
+    fn get(&self, position: Position) -> Option<char> {
+        if position.0 >= 0 && position.0 < self.height && position.1 >= 0 && position.1 < self.width
+        {
+            let c = self.data[position.0 as usize][position.1 as usize];
+            Some(c)
+        } else {
+            None
+        }
     }
 
-    fn go_to(&self, position: Position, direction: Direction) -> Option<Position> {
+    fn go_to(&self, position: Position, direction: Direction) -> Position {
         match direction {
-            Direction::Top => {
-                if position.1 > 0 {
-                    Some(Position(position.0, position.1 - 1))
-                } else {
-                    None
-                }
-            }
-            Direction::Down => {
-                if position.1 < self.height - 1 {
-                    Some(Position(position.0, position.1 + 1))
-                } else {
-                    None
-                }
-            }
-            Direction::Left => {
-                if position.0 > 0 {
-                    Some(Position(position.0 - 1, position.1))
-                } else {
-                    None
-                }
-            }
-            Direction::Right => {
-                if position.0 < self.width - 1 {
-                    Some(Position(position.0 + 1, position.1))
-                } else {
-                    None
-                }
-            }
-            Direction::TopLeft => {
-                if position.0 > 0 && position.1 > 0 {
-                    Some(Position(position.0 - 1, position.1 - 1))
-                } else {
-                    None
-                }
-            }
-            Direction::TopRight => {
-                if position.0 < self.width - 1 && position.1 > 0 {
-                    Some(Position(position.0 + 1, position.1 - 1))
-                } else {
-                    None
-                }
-            }
-            Direction::DownLeft => {
-                if position.0 > 0 && position.1 < self.height - 1 {
-                    Some(Position(position.0 - 1, position.1 + 1))
-                } else {
-                    None
-                }
-            }
-            Direction::DownRight => {
-                if position.0 < self.width - 1 && position.1 < self.height - 1 {
-                    Some(Position(position.0 + 1, position.1 + 1))
-                } else {
-                    None
-                }
-            }
+            Direction::Top => Position(position.0, position.1 - 1),
+            Direction::Down => Position(position.0, position.1 + 1),
+            Direction::Left => Position(position.0 - 1, position.1),
+            Direction::Right => Position(position.0 + 1, position.1),
+            Direction::TopLeft => Position(position.0 - 1, position.1 - 1),
+            Direction::TopRight => Position(position.0 + 1, position.1 - 1),
+            Direction::DownLeft => Position(position.0 - 1, position.1 + 1),
+            Direction::DownRight => Position(position.0 + 1, position.1 + 1),
         }
     }
 }
