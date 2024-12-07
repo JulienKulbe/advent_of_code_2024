d71edafa16741beae73dc8e1af494442e154906d
Julien Kulbe <julien.kulbe@googlemail.com>
2024-12-07 15:46:13 +0100

    day 7, star 2

--- advent_of_code_07/src/main.rs
+++ advent_of_code_07/src/main.rs
@@ -5,8 +5,8 @@ fn main() -> Result<()> {
     let sum = get_calibration_results("input.txt")?;
     println!("Calibration results: {sum}");
 
-    let score = calculate_similarity_score("input.txt")?;
-    println!("Similarity score: {score}");
+    let sum = get_calibration_results_with_concatenation("input.txt")?;
+    println!("Calibration results: {sum}");
 
     Ok(())
 }
@@ -22,8 +22,15 @@ fn get_calibration_results(filename: &str) -> Result<u64> {
     Ok(sum)
 }
 
-fn calculate_similarity_score(filename: &str) -> Result<u32> {
-    Ok(1)
+fn get_calibration_results_with_concatenation(filename: &str) -> Result<u64> {
+    let equations = parse_file(filename)?;
+    let sum = equations
+        .iter()
+        .filter(|&equation| is_valid_equation_with_concatenation(equation))
+        .map(|equation| equation.test_value)
+        .sum();
+
+    Ok(sum)
 }
 
 fn parse_file(filename: &str) -> Result<Vec<Equation>> {
@@ -64,6 +71,41 @@ fn is_valid_equation(equation: &Equation) -> bool {
     }
 }
 
+fn is_valid_equation_with_concatenation(equation: &Equation) -> bool {
+    if equation.values.len() == 1 {
+        equation.test_value == equation.values[0]
+    } else if equation.test_value < equation.values[0] {
+        false
+    } else {
+        let mut equation_mult = equation.clone();
+        let mut equation_add = equation.clone();
+        let mut equation_con = equation.clone();
+
+        let mult_result = equation_mult.values.remove(0) * equation_mult.values.remove(0);
+        let add_result = equation_add.values.remove(0) + equation_add.values.remove(0);
+        let con_result =
+            concatenate_values(equation_con.values.remove(0), equation_con.values.remove(0));
+
+        equation_mult.values.insert(0, mult_result);
+        equation_add.values.insert(0, add_result);
+        equation_con.values.insert(0, con_result);
+
+        is_valid_equation_with_concatenation(&equation_mult)
+            || is_valid_equation_with_concatenation(&equation_add)
+            || is_valid_equation_with_concatenation(&equation_con)
+    }
+}
+
+fn concatenate_values(a: u64, b: u64) -> u64 {
+    let mut result = a;
+    let mut temp = b;
+    while temp > 0 {
+        temp /= 10;
+        result *= 10;
+    }
+    result + b
+}
+
 #[derive(Debug, Clone)]
 struct Equation {
     test_value: u64,
@@ -89,17 +131,16 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "reason"]
     fn test_small_b() {
-        let result = calculate_similarity_score("input_small.txt");
+        let result = get_calibration_results_with_concatenation("input_small.txt");
         assert!(result.is_ok());
-        assert_eq!(31, result.unwrap())
+        assert_eq!(11387, result.unwrap())
     }
 
     #[test]
     #[ignore = "reason"]
     fn test_input_b() {
-        let result = calculate_similarity_score("input.txt");
+        let result = get_calibration_results_with_concatenation("input.txt");
         assert!(result.is_ok());
         assert_eq!(20351745, result.unwrap())
     }
